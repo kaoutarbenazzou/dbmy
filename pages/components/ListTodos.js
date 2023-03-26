e2d4ff00c69113d58e1046a477d1cbdc8b3d2abc
Kaoutar Benazzou <94478035+kaoutarbenazzou@users.noreply.github.com>
2023-03-26 17:03:50 +0000

    Add files via upload

---
+++ pages/components/ListTodos.js
@@ -0,0 +1,141 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+
+
+
+import EditTodo from "./EditTodo";
+
+
+
+
+const ListTodos = () => {
+
+    const [todos, setTodos] = useState([]);
+
+
+
+
+
+    const deleteTodo = async id => {
+
+        console.log("the item to delete is " + id)
+
+        try {
+
+            const deleteTodo = await fetch(`http://localhost:3001/api/todo/${id}`, {
+
+                method: "DELETE"
+
+            });
+
+
+
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+
+        } catch (err) {
+
+            console.error(err.message);
+
+        }
+
+    };
+
+
+
+
+    const getTodos = async () => {
+
+        try {
+
+            const response = await fetch("http://localhost:3001/api/todos");
+
+            const jsonData = await response.json();
+
+
+
+
+            setTodos(jsonData);
+
+        } catch (err) {
+
+            console.error(err.message);
+
+        }
+
+    };
+
+
+
+
+    useEffect(() => {
+
+        getTodos();
+
+    }, []);
+
+
+    return (
+
+        <Fragment>
+
+            {" "}
+
+            <table className="table mt-5 text-center">
+
+                <thead>
+
+                    <tr>
+
+                        <th>Description</th>
+
+                        <th>Edit</th>
+
+                        <th>Delete</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {todos.map((todo) => (
+
+                        <tr key={todo.todo_id}>
+
+                            <td>{todo.description}</td>
+
+                            <td>
+
+                                <EditTodo todo={todo} />
+
+                            </td>
+
+                            <td>
+
+                                <button className="btn btn-outline-danger" onClick={() => deleteTodo(todo.todo_id)}>
+
+                                    Delete
+
+                                </button>
+
+                            </td>
+
+                        </tr>
+
+                    ))}
+
+                </tbody>
+
+            </table>
+
+        </Fragment>
+
+    );
+
+};
+
+
+
+
+export default ListTodos;
